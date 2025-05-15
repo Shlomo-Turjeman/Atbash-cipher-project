2a218114f097ac8606c983a8de230e5b39aef114
Shlomo <shlomot575@gmail.com>
2025-05-15 11:21:42 +0300

    add Atbash decrypt function

--- Atbash cipher project/Program.cs
+++ Atbash cipher project/Program.cs
@@ -8,10 +8,30 @@ namespace Atbash_cipher_project
 {
     internal class Program
     {
-
-        
-        static void Main(string[] args)
+        static string AtbashSDecrypt(string text)
         {
+            string result = "";
+            char decrypt;
+
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c))
+                {
+                    decrypt = (char) ('z' - (char.ToLower(c) - 'a'));
+                    result += decrypt;
+                }
+                else
+                {
+                    result += c;   
+                }
+            }
+            
+            return result;
+        }
+
+            static void Main(string[] args)
+            {
+
+            }
         }
     }
-}
