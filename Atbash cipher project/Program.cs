c454e60f1939523517d235b8a56fee4bdceb1bc9
Shlomo <shlomot575@gmail.com>
2025-05-15 12:24:21 +0300

    add decrypt function'

--- Atbash cipher project/Program.cs
+++ Atbash cipher project/Program.cs
@@ -10,43 +10,27 @@ namespace Atbash_cipher_project
     internal class Program
     {
 
-        static int WarningCounter(string text)
+        static string AtbashSDecrypt(string text)
         {
-            string[] dangerousWords = { "bomb", "nukhba", "fighter", "rocket", "secret" };
-            string[] splitText = text.Split(' ');
-            int point = 0;
+            string result = "";
+            char decrypt;
 
-            foreach (string word in splitText)
+            foreach (char c in text)
             {
-                if (dangerousWords.Contains(word))
+                if (char.IsLetter(c))
                 {
-                    point++;
+                    decrypt = (char)('z' - (char.ToLower(c) - 'a'));
+                    result += decrypt;
+                }
+                else
+                {
+                    result += c;
                 }
             }
-            return point;
+
+            return result;
         }
-        
-        static void FinalPrint(string text, int point)
-        {
-            string warning = "";
-
-            if (point >= 1 && point <= 5)
-            {
-                warning = "WARNING";
-            }
-            else if (point >= 6 && point <= 10)
-            {
-                warning = "DANGER";
-            }
-            else if (point >= 11 && point <= 15)
-            {
-                warning = "ULTRA DANGER";
-            }
-
-            Console.WriteLine($"Message: {text} \nWorning: {warning} \nTotal points: {point}");
-        }
-
-            static void Main(string[] args)
+        static void Main(string[] args)
             {
 
             }
